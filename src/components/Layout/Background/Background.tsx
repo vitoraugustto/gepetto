58b4dcea172b830db4db92f0beab5814220431ab
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-05-16 01:09:41 -0300

    feat: change backgroundColor of Background component to white

--- src/components/Layout/Background/Background.tsx
+++ src/components/Layout/Background/Background.tsx
@@ -1,10 +1,11 @@
 import { Box } from '@components';
+import { white } from '@themes/default/colors';
 
 import { IBackground } from './Background.types';
 
 export const Background: React.FC<IBackground> = ({ children }) => {
   return (
-    <Box py="24px" px="12%" backgroundColor="#eef7f4" minHeight={'100vh'}>
+    <Box py="24px" px="12%" backgroundColor={white} minHeight={'100vh'}>
       <>{children}</>
     </Box>
   );
