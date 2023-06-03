6c5804e059f7209d099beaba406cab1fe28d7928
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-06-02 23:43:49 -0300

    chore: change from triage to medical-chat in services/gpt.ts

--- src/services/gpt.ts
+++ src/services/gpt.ts
@@ -1,9 +1,9 @@
 import { instance } from './axios';
 
-export const fetchContext = () => instance({ url: '/api/v1/triage' });
+export const fetchContext = () => instance({ url: '/api/v1/medical-chat' });
 
 export const clearContext = () =>
-  instance({ url: '/api/v1/triage', method: 'PUT' });
+  instance({ url: '/api/v1/medical-chat', method: 'PUT' });
 
 export const sendPrompt = (prompt: string) =>
-  instance({ url: '/api/v1/triage', method: 'POST', data: { prompt } });
+  instance({ url: '/api/v1/medical-chat', method: 'POST', data: { prompt } });
