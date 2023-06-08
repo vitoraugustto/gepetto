0261d5033b5549d0d7a9d506d340cb6993842c46
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-06-07 23:38:26 -0300

    feat: add primary color in GettingStartedScreen 'Gepetto'

--- src/screens/GettingStartedScreen.tsx
+++ src/screens/GettingStartedScreen.tsx
@@ -1,11 +1,19 @@
 import { Background, Box, Button, Text } from '@components';
+import { useTheme } from '@mui/material';
 
 export const GettingStartedScreen = () => {
+  const theme = useTheme();
+
   return (
     <Background>
       <Box style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
         <Box gap="32px">
-          <Text fontSize="72px" letterSpacing="9.5px" variant="h1">
+          <Text
+            fontSize="72px"
+            letterSpacing="9.5px"
+            variant="h1"
+            color={theme.palette.primary[700]}
+          >
             Gepetto
           </Text>
           <Button to="/context" text="Começar consulta médica 🤖🎉" />
