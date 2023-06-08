b00915dba4a75b871cde3b6e1b8eed68ce002b4e
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-06-07 23:37:11 -0300

    feat: add backgroundColor from useTheme in Background component

--- src/components/Layout/Background/Background.tsx
+++ src/components/Layout/Background/Background.tsx
@@ -1,11 +1,18 @@
 import { Box } from '@components';
-import { white } from '@themes/default/colors';
+import { useTheme } from '@mui/material';
 
 import { IBackground } from './Background.types';
 
 export const Background: React.FC<IBackground> = ({ children }) => {
+  const theme = useTheme();
+
   return (
-    <Box py="24px" px="12%" backgroundColor={white} minHeight={'100vh'}>
+    <Box
+      py="24px"
+      px="12%"
+      backgroundColor={theme.backgroundColor}
+      minHeight={'100vh'}
+    >
       <>{children}</>
     </Box>
   );
