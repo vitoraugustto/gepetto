3880832ea75fda49557fe2e0c913fa873c378628
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-11-22 23:54:38 -0300

    fix: remove shadows MUI override in default theme

--- src/themes/default/index.ts
+++ src/themes/default/index.ts
@@ -1,11 +1,9 @@
-import { Shadows, createTheme } from '@mui/material';
+import { createTheme } from '@mui/material';
 import { pink, purple } from '@mui/material/colors';
-import { shadows } from '@mui/system';
 
 import { black, gray } from '../colors';
 
 export const defaultTheme = createTheme({
-  shadows: Array(shadows.length).fill('none') as Shadows,
   breakpoints: {
     values: {
       xs: 0,
